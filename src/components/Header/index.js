83bc64f36f44992f6fc78cd5a5ce5d881c730ec5
Aylin McGinnis <amcginnis@rsglab.com>
2020-07-31 14:31:05 -0400

    Add beta badge

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,23 +1,22 @@
-import { Link } from "gatsby"
-import PropTypes from "prop-types"
-import React from "react"
+import { Link } from 'gatsby';
+import PropTypes from 'prop-types';
+import React from 'react';
 
-import './Header.less'
+import { Badge } from 'shards-react';
+
+import './Header.less';
 
 const Header = ({ siteTitle }) => (
   <header>
-      <h1 >
-        <Link
-          to="/"
-        >
-          {siteTitle}
-        </Link>
-      </h1>
+    <Badge theme="dark">Beta</Badge>
+    <h1>
+      <Link to="/">{siteTitle}</Link>
+    </h1>
   </header>
-)
+);
 
 Header.propTypes = {
   siteTitle: PropTypes.string,
-}
+};
 
-export default Header
+export default Header;
