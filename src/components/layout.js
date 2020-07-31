f503ef8259c3238778a2911b4de3032432ed83ef
Aylin McGinnis <amcginnis@rsglab.com>
2020-07-31 14:07:34 -0400

    Update footer

--- src/components/layout.js
+++ src/components/layout.js
@@ -5,36 +5,40 @@
  * See: https://www.gatsbyjs.org/docs/use-static-query/
  */
 
-import React from "react"
-import PropTypes from "prop-types"
+import React from 'react';
+import PropTypes from 'prop-types';
 
-import Header from "./Header/"
+import Header from './Header/';
 
-import "bootstrap/dist/css/bootstrap.min.css";
-import "shards-ui/dist/css/shards.min.css"
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'shards-ui/dist/css/shards.min.css';
 
-import "./layout.less"
+import './layout.less';
 
 const Layout = ({ children }) => {
-
   return (
     <>
       <Header siteTitle="blackartisans" />
-      <div
-      >
+      <div>
         <main>{children}</main>
-        <footer>
+        <footer className="footer">
           © {new Date().getFullYear()}, Built with
           {` `}
-          <a href="https://www.gatsbyjs.org">Gatsby</a>
+          <a href="https://www.gatsbyjs.org" target="_blank">
+            Gatsby
+          </a>{' '}
+          by{' '}
+          <a href="https://www.aylinmarie.co" target="_blank">
+            Aylin Marie
+          </a>
         </footer>
       </div>
     </>
-  )
-}
+  );
+};
 
 Layout.propTypes = {
   children: PropTypes.node.isRequired,
-}
+};
 
-export default Layout
+export default Layout;
